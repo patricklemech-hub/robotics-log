6203fa1c429c16b20b389c8da64978847e6454b0
patricklemech-hub <patrick.le.mech@gmail.com>
2026-08-27 00:16:09 -0700

    def function and return

--- 01-python-foundations/practice/03-define-function.py
+++ 01-python-foundations/practice/03-define-function.py
@@ -1,15 +1,17 @@
 #to define a function, we use def as the define funciton to save the function sersve as storage to bring out as needed 
-def multiply():
-    #ask user to input value x and y 
-    x = float(input("what is the x value:"))
-    y = float(input("what is the y value:"))
-    
-    #use the input value to execute the calculation
-    z = x*y
-    z = round(float(z),2)
-    #return the value 
-    return z 
-    
+#function that receive input value 
+def value():
+    x = input("what is the value of x:")
+    y = input("what is the value of y:")
+    return int(x), int(y)
 
-multiply()
+
+#function that do calculation 
+def multiply(a,b):
+    a*b
+    return a*b
+
+x,y =value()
+answer= multiply(x,y)
+print("The multiplier of 2 input values is ",answer)
 
