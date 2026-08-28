96f1d7490364242b80df5fd05c91c5ca3a3ec103
patricklemech-hub <patrick.le.mech@gmail.com>
2026-08-27 18:33:21 -0700

    update

--- 01-python-foundations/practice/05-bool,%,even or odd.py
+++ 01-python-foundations/practice/05-bool,%,even or odd.py
@@ -10,6 +10,13 @@ def is_even(x):
         return True
     else:
         return False
+"""
+ different way to write this 
+    return True if x%2 == 0 else False 
+    
+ The most clean is: 
+    return (x%2==0) , meaning that if x%2 == 0 it will return true else it false because if x is odd number therefore oddnumber %2 cant be ==0    
+"""
     
 
 num= int(input("what is the number?:"))
