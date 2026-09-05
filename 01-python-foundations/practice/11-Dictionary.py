ad32ae566a2c1a0bdc3bd242ab28f2ea44092ad1
patricklemech-hub <patrick.le.mech@gmail.com>
2026-09-05 10:02:51 -0700

    Update

--- 01-python-foundations/practice/11-Dictionary.py
+++ 01-python-foundations/practice/11-Dictionary.py
@@ -9,7 +9,14 @@ student = {
     "broccoli": "vegetable",
 }
 
-for name  in student:
-    print(name, student[name], sep="->") #This command will print the key-value pairs in the dictionary. The key is the name of the item, and the value is the type of item (fruit or vegetable). For example, mango is a fruit, carrot is a vegetable, and so on
-    #we also modified the spacing fromm blank space to "->" using the sep parameter in the print function. The sep parameter specifies the string that is inserted between the items to be printed. In this case, we used "->" to separate the key and value in the output.
+def nameprint():
+    for name in student:
+         return print(name, student[name], sep="->") #This command will print the key-value pairs in the dictionary. The key is the name of the item, and the value is the type of item (fruit or vegetable). For example, mango is a fruit, carrot is a vegetable, and so on
+        #we also modified the spacing fromm blank space to "->" using the sep parameter in the print function. The sep parameter specifies the string that is inserted between the items to be printed. In this case, we used "->" to separate the key and value in the output.
+
+def test():
+    for test in student: 
+        return print(test) #This command will print the key-value pairs in the dictionary. The key is the name of the item, and the value is the type of item (fruit or vegetable). For example, mango is a fruit, carrot is a vegetable, and so on
     
+if print(student["Mana"]) is False:
+    print("Mana is not in the dictionary") #This command will print a message indicating that "Mana" is not a key in the dictionary. Since "Mana" is not present in the dictionary, the output will be "Mana is not in the dictionary".
